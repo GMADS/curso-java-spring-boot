86605ff10642010788de419cda07ea3202cd04a9
Guilherme Miguel <guilherme.souza1996@hotmail.com>
2026-06-13 21:54:15 -0300

    Finalizando a implementação dos endpoints de crud.

--- produtosapi/produtosapi/src/main/java/github/gmads/produtosapi/controller/ProdutoController.java
+++ produtosapi/produtosapi/src/main/java/github/gmads/produtosapi/controller/ProdutoController.java
@@ -4,6 +4,8 @@ import github.gmads.produtosapi.model.Produto;
 import github.gmads.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -33,5 +35,27 @@ public class ProdutoController {
     @DeleteMapping("/{id}")
     public void deletarPorId(@PathVariable String id){
         produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void atualizaProduto(@PathVariable("id") String id,
+                                @RequestBody Produto produtoAtualizado){
+        produtoAtualizado.setId(id);
+        produtoRepository.save(produtoAtualizado);
+    }
+
+    @GetMapping
+    public List<Produto> ObterByParameter(@RequestParam("nome") String nome,
+                                          @RequestParam("descricao") String descricao){
+
+        if(nome != null && !nome.isBlank()){
+            return produtoRepository.findByNome(nome);
+        }
+
+        if(descricao != null && !descricao.isBlank()){
+            return produtoRepository.findByDescricao(descricao);
+        }
+
+        return new ArrayList<Produto>();
     }
 }
--- produtosapi/produtosapi/src/main/java/github/gmads/produtosapi/repository/ProdutoRepository.java
+++ produtosapi/produtosapi/src/main/java/github/gmads/produtosapi/repository/ProdutoRepository.java
@@ -3,5 +3,11 @@ package github.gmads.produtosapi.repository;
 import github.gmads.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+
 public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+    List<Produto> findByNome(String nome);
+    List<Produto> findByDescricao(String descricao);
 }
